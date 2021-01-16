d3e0079a0038e32481e92fd9ad3082c8ac237839
Nutticha <nutticha30234@gmail.com>
2021-01-16 20:45:08 +0700

    update

--- Homework_4.java
+++ Homework_4.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Homework_4 {
     float salary;
     
-    public Home(float hours, float rate) {
+    /*public Home(float hours, float rate) {
         Scanner scan = new Scanner(System.in);
         
         System.out.print("Enter hours work : ");
@@ -16,11 +16,11 @@ public class Homework_4 {
         System.out.print("Enter hours work : ");
         hours = scan.nextFloat();
         System.out.print("Enter hourly rate : ");
-        rate = scan.nextFloat();*/
+        rate = scan.nextFloat();
         Homework_4 home = new Home( );
 
         salary = (hours <= 40) ? (hours*rate) :
         (rate * (1.5f * hours - 20));
         System.out.println("Employee get salary : " + salary);
-        }
+        }*/
 }
